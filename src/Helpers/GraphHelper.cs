cbfddaec98e67a15915fe7d0a5de24350edaa3c6
savery <14942732+stevenAvery@users.noreply.github.com>
2020-02-10 20:52:04 -0500

    Cleaned up GraphHelper and fixed issue with cycles in graph

--- src/Helpers/GraphHelper.cs
+++ src/Helpers/GraphHelper.cs
@@ -5,6 +5,15 @@ using System.Linq;
 namespace CheapFlights.Helpers {
     public static class GraphHelpers {
         /// <summary>
+        /// Node in an Adjacency List.
+        /// </summary>
+        /// <typeparam name="TEdge">Edge type.</typeparam>
+        public class AdjacencyNode<TEdge> {
+            public string Destination { get; set; }
+            public TEdge Edge { get; set; }
+        }
+
+        /// <summary>
         /// Converts list of edges (specifically flights) into an adjacency list.
         /// </summary>
         /// <typeparam name="TEdge">Edge type.</typeparam>
@@ -12,7 +21,7 @@ namespace CheapFlights.Helpers {
         /// <param name="getFrom">Function to get origin id of a given TEdge.</param>
         /// <param name="getTo">Function to get destination id of a given TEdge.</param>
         /// <returns>Adjacency list representation of graph.</returns>
-        public static Dictionary<string, List<(string, TEdge)>> ToAdjacencyList<TEdge>(
+        public static Dictionary<string, List<AdjacencyNode<TEdge>>> ToAdjacencyList<TEdge>(
             this IEnumerable<TEdge> edges, Func<TEdge, string> getFrom, Func<TEdge, string> getTo) {
 
             // get list of all distinct vertices
@@ -21,13 +30,17 @@ namespace CheapFlights.Helpers {
             var distinctVertices = fromVertices.Concat(toVertices).Distinct();
 
             // initialize adjList, so that every distinct vertex has an empty list of adjacent vertices
-            var adjList = new Dictionary<string, List<(string, TEdge)>>();
+            var adjList = new Dictionary<string, List<AdjacencyNode<TEdge>>>();
             foreach (var vertex in distinctVertices)
-                adjList.Add(vertex, new List<(string, TEdge)>());
+                adjList.Add(vertex, new List<AdjacencyNode<TEdge>>());
 
             // add every edge to adjacency list
-            foreach(var edge in edges)
-                adjList[getFrom(edge)].Add((getTo(edge), edge));
+            foreach(var edge in edges) {
+                adjList[getFrom(edge)].Add(new AdjacencyNode<TEdge> () {
+                    Destination = getTo(edge),
+                    Edge = edge
+                });
+            }
 
             return adjList;
         }
@@ -41,7 +54,7 @@ namespace CheapFlights.Helpers {
         /// <param name="target">Id of the target vertex.</param>
         /// <returns>All paths from source to target.</returns>
         public static List<List<TEdge>> AllPaths<TEdge>(
-            this Dictionary<string, List<(string, TEdge)>> adjList,
+            this Dictionary<string, List<AdjacencyNode<TEdge>>> adjList,
             string source, string target) {
 
             if (!adjList.ContainsKey(source))
@@ -50,7 +63,7 @@ namespace CheapFlights.Helpers {
                 throw new ArgumentException($"{nameof(target)} not found in {nameof(adjList)}", nameof(target));
 
             // all paths that have been discovered
-            var allPaths = new List<List<(string, TEdge)>>();
+            var allPaths = new List<List<AdjacencyNode<TEdge>>>();
 
             // initialize all vertices as not visited
             var visited = new Dictionary<string, bool>();
@@ -58,23 +71,28 @@ namespace CheapFlights.Helpers {
                 visited.Add(key, false);
 
             // maintain a queue of currently discovered verticies
-            var discoveredVertices = new Queue<List<(string, TEdge)>>();
-            discoveredVertices.Enqueue(new List<(string, TEdge)>() { (source, default(TEdge)) });
+            var discoveredVertices = new Queue<List<AdjacencyNode<TEdge>>>();
+            discoveredVertices.Enqueue(new List<AdjacencyNode<TEdge>>() { 
+                new AdjacencyNode<TEdge> () {
+                    Destination = source,
+                    Edge = default(TEdge)
+                }
+            });
 
-            while (discoveredVertices.TryDequeue(out List<(string, TEdge)> current)) {
-                foreach (var adjacent in adjList[current.Last().Item1]) {
+            while (discoveredVertices.TryDequeue(out List<AdjacencyNode<TEdge>> current)) {
+                foreach (var adjacent in adjList[current.Last().Destination]) {
                     var path = current.Append(adjacent).ToList();
                     // if we're at the target, add the cuurent path to resulting list of paths
-                    if (adjacent.Item1 == target)
+                    if (adjacent.Destination == target)
                         allPaths.Add(path.Skip(1).ToList());
                     // if we've never visited the adjacent vertex, add it to list of discovered vertices
-                    if (!visited[adjacent.Item1])
+                    else if (!visited[adjacent.Destination])
                         discoveredVertices.Enqueue(path);
                 }
-                visited[current.Last().Item1] = true;
+                visited[current.Last().Destination] = true;
             }
 
-            return allPaths.Select(path => path.Select(elem => elem.Item2).ToList()).ToList();
+            return allPaths.Select(path => path.Select(elem => elem.Edge).ToList()).ToList();
         }
     }
 }
